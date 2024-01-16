994f1ea4aaf185ccd4048f6676aa709534e87c22
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-16 21:26:31 +0530

    adding Adv Template

---
+++ Templates/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll LLINF = LLONG_MAX;
+
+void solve() {
+    // Your code goes here
+}
+
+int main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
