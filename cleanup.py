1023cb8986255d3a09b7b3a05ae68d2dd86ef1e1
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-16 21:08:29 +0530

    Adding Clean Up Script

---
+++ cleanup.py
@@ -0,0 +1,15 @@
+# delete all the .exe files in the current directory and all subdirectories
+import os
+import sys
+
+def cleanup(path):
+    if os.path.isfile(path):
+        if path.endswith(".exe"):
+            os.remove(path)
+            print("Deleted", path)
+    else:
+        for filename in os.listdir(path):
+            cleanup(os.path.join(path, filename))
+            
+if __name__ == "__main__":
+    cleanup(sys.argv[1] if len(sys.argv) > 1 else ".")
