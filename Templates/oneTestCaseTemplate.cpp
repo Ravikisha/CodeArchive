51ce9ceca432f31c172a4121c73916735b706237
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-16 21:40:55 +0530

    Adding oneTestCase Template

---
+++ Templates/oneTestCaseTemplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll LLINF = LLONG_MAX;
+
+void solve() {
+    // Solve Problem Here
+}
+
+int main() {
+    fastio;
+
+    // Call the solve function for one test case
+    solve();
+
+    return 0;
+}
