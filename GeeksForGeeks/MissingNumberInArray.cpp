0c6564e6f4e717badae3311e82ff8ee916ebb622
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-02 21:43:50 +0530

    Missing Number in Array by Bitwise XOR

--- GeeksForGeeks/MissingNumberInArray.cpp
+++ GeeksForGeeks/MissingNumberInArray.cpp
@@ -39,3 +39,50 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+
+// Solution For Same using BitWise XOR
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int missingNumber(vector<int>& array, int n) {
+        int x1 = array[0];
+        for(int i=1;i < n-1;i++){
+            x1 ^= array[i];
+        }
+        int x2 = 1;
+        for(int i=2;i<=n;i++){
+            x2 ^= i;
+        }
+        return x1 ^ x2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.missingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
