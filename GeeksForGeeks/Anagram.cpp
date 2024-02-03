19aca90b5d94d8707643a49c51f3df55b21b2579
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-03 22:19:50 +0530

    anagram optimized solution

--- GeeksForGeeks/Anagram.cpp
+++ GeeksForGeeks/Anagram.cpp
@@ -36,3 +36,53 @@ int main() {
 }
 
 // } Driver Code Ends
+
+// Optimized solution
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        int chars[26];
+        memset(chars, 0, sizeof(chars));
+        for(auto val: a)
+            chars[val - 'a']++;
+        for(auto val: b)
+            chars[val - 'a']--;
+        for(auto val: chars){
+            if(val != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    int t;
+
+    cin >> t;
+
+    while(t--){
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if(obj.isAnagram(c, d)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+}
+
+// } Driver Code Ends
