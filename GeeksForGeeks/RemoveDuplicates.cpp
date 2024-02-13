b74e62d6e2546fff012eaa67ef2708ce19b558d8
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-13 22:32:20 +0530

    Remove Duplicates

---
+++ GeeksForGeeks/RemoveDuplicates.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    string removeDups(string S)
+    {
+        int arr[26] = {0};
+        string result = "";
+        for (int i = 0; i < S.length(); i++)
+        {
+            if (arr[S[i] - 'a'] == 0)
+            {
+                result += S[i];
+                arr[S[i] - 'a']++;
+            }
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
