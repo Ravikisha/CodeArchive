cc8b15aee2bc33d1f0a8880342feff88ede217f2
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-15 22:38:17 +0530

    Find duplicates in an array

---
+++ GeeksForGeeks/FindDuplicatesInAnArray.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(long long arr[], int n) {
+        int count=0;
+        //raverse the given array from i= 0 to n-1 elements
+        //  Go to index arr[i]%n and increment its value by n.
+        for(int i=0;i<n;i++){
+            int index=arr[i]%n;
+            arr[index]+=n;
+        }
+        vector<int>ans;
+        // Now traverse the array again and print all those 
+        // indexes i for which arr[i]/n is greater than 1
+        for(int i=0;i<n;i++){
+            if(arr[i]/n>=2){
+                ans.push_back(i);
+                count++;
+            }
+        }
+        if(count==0)ans.push_back(-1);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
