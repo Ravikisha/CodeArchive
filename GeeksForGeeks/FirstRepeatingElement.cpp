65d2c6baf61977eeb1c4ab5277b4f555597c3c0f
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-22 23:08:47 +0530

    First Repeating Element

---
+++ GeeksForGeeks/FirstRepeatingElement.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution
+{
+public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n)
+    {
+        unordered_map<int, int> umap;
+        for (int i = 0; i < n; i++)
+        {
+            umap[arr[i]]++;
+        }
+        int flag = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (umap[arr[i]] > 1)
+            {
+                return i + 1;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == -1)
+            return flag;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i)
+            cin >> arr[i];
+        Solution ob;
+        cout << ob.firstRepeated(arr, n) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
