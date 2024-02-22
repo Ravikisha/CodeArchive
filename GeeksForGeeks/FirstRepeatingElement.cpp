cf4a7755f3470f017a7205aa69096e7db4ee215a
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-22 23:10:39 +0530

    Update First Repeating Element

--- GeeksForGeeks/FirstRepeatingElement.cpp
+++ GeeksForGeeks/FirstRepeatingElement.cpp
@@ -18,18 +18,14 @@ public:
         {
             umap[arr[i]]++;
         }
-        int flag = -1;
         for (int i = 0; i < n; i++)
         {
             if (umap[arr[i]] > 1)
             {
                 return i + 1;
-                flag = 1;
-                break;
             }
         }
-        if (flag == -1)
-            return flag;
+        return -1;
     }
 };
 
