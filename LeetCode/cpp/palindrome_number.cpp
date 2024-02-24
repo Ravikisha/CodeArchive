d3d41824c72bb8b08ecb76b1bce509b7b3a173df
ravikishan.69 <ravikishan63392@gmail.com>
2024-02-24 15:14:47 +0530

    Palindrome Number

--- LeetCode/cpp/palindrome_number.cpp
+++ LeetCode/cpp/palindrome_number.cpp
@@ -12,3 +12,17 @@ class Solution {
     return x == revertedNumber || x == revertedNumber / 10; 
     }
 }
+
+// cpp result
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long long pali = 0, temp = x;
+        while(temp != 0){
+            pali = pali*10+(temp%10);
+            temp /= 10;
+        }   
+        return pali==x;
+    }
+};
