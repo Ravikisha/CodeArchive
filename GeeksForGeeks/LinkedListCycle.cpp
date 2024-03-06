58e1d9077c8d8c37ab39951e0e67de7d997e45d1
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-06 21:20:27 +0530

    Linked List Cycle

---
+++ GeeksForGeeks/LinkedListCycle.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (!head)
+            return 0;
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast)
+        {
+            if (fast == slow)
+                return 1;
+
+            slow = slow->next;
+            fast = fast->next ? (fast->next)->next : fast->next;
+        }
+
+        return 0;
+    }
+};
