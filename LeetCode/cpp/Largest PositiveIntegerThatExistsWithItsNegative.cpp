b493cd4a23faf40fa4eebd7977d79d5ce3db4493
ravikishan.69 <ravikishan63392@gmail.com>
2024-05-02 23:00:08 +0530

    Largest Positive Integer That Exists With Its Negative

---
+++ LeetCode/cpp/Largest PositiveIntegerThatExistsWithItsNegative.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        unordered_set<int> lookup(cbegin(nums), cend(nums));
+        int result = -1;
+        for (const auto& x : lookup) {
+            if (x > result && lookup.count(-x)) {
+                result = x;
+            }
+        }
+        return result;
+    }
+};
