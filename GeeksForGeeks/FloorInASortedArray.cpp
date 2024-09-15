9f1795dd3e6d0633db4dfbbd89a9c350e27d16c9
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-15 11:37:28 +0530

    Floor in a Sorted Array (Iteratively)

--- GeeksForGeeks/FloorInASortedArray.cpp
+++ GeeksForGeeks/FloorInASortedArray.cpp
@@ -75,3 +75,70 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+
+/*
+Alternative Solution (Iteratively):
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> &v, long long n, long long x) {
+        int high = n-1;
+        int low = 0;
+        int result = n;
+        // if the target is smaller than the first element in array
+        if(x < v[0]) return -1;
+        // if the last element is smaller than target
+        if(v[n-1] < x) return n-1;
+        
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(v[mid] <= x && x < v[mid+1]){
+                return mid;
+            }else if(v[mid+1] <= x) low = mid+1;
+            else if(v[mid] > x) high = mid-1;
+        }
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    long long t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long x;
+        cin >> x;
+
+        vector<long long> v;
+
+        for (long long i = 0; i < n; i++) {
+            long long temp;
+            cin >> temp;
+            v.push_back(temp);
+        }
+        Solution obj;
+        cout << obj.findFloor(v, n, x) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
+*/
