02d99df108531bd7bf0c1bc9974ba933935b3f26
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-16 22:56:37 +0530

    Indexes of Subarray Sum

---
+++ GeeksForGeeks/IndexesOfSubarraySum.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int> arr, int n, long long s) {
+        int start=0, end=0;
+        int curr = 0;
+        
+        for(int i=0;i<n;i++){
+            curr += arr[i];
+            
+            if(curr >= s){
+                end=i;
+                
+                while(curr > s && start < end){
+                    curr -= arr[start];
+                    ++start;
+                }
+                
+                if(curr == s){
+                    return {start+1, end+1};
+                }
+            }
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        long long s;
+        cin >> n >> s;
+        vector<int> arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> res;
+        res = ob.subarraySum(arr, n, s);
+
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
