a4b894c86daa5cc769a0b91ff899aa251cf39079
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-22 01:34:22 +0530

    Is Subsequence

---
+++ LeetCode/cpp/IsSubsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.length() == 0) return true;
+        int idx = 0;
+        for(const char& c: t){
+            if(c == s[idx]){
+                idx++;
+            }
+            if(idx == s.length()) return true;
+        }
+        return false;
+    }
+    
+};
