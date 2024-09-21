7c4642c2f662cd24f9dd6876017fbed09d11cd16
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-21 23:17:27 +0530

    Merge Strings Alternately

---
+++ LeetCode/cpp/MergeStringsAlternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    Solution(){
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+    }
+    string mergeAlternately(string word1, string word2) {
+        int index = 0;
+        string result = "";
+        while(index < word1.size() && index < word2.size()){
+            result += word1[index];
+            result += word2[index];
+            index++;
+        }
+        while(index < word1.size()){
+            result += word1[index];
+            index++;
+        }
+        while(index < word2.size()){
+            result += word2[index];
+            index++;
+        }
+
+        return result;
+    }
+};
