7d3f11b701c29fd40ed133826acdd387dabc1ba1
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-23 23:44:12 +0530

    Longest Consecutive Sequence

---
+++ LeetCode/cpp/LongestConsecutiveSequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> uset(nums.begin(), nums.end());
+        int longest = 0;
+        int n = nums.size();
+        if(n == 0) return n;
+        
+        for(int ele: uset){
+            if(uset.find(ele -1) == uset.end()){
+                int count = 1;
+                int x = ele;
+                while(uset.find(x +1) != uset.end()){
+                    x = x+1;
+                    count++;
+                }
+                longest = max(count, longest);
+            }
+        }
+        return longest;
+    }
+};
