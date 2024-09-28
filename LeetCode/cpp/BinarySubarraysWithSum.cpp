6952e05ec149fe6983f8a1e1c8c1a058d37723ad
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-28 17:13:09 +0530

    930. Binary Subarrays With Sum (Optimal using Sliding WIndow)

--- LeetCode/cpp/BinarySubarraysWithSum.cpp
+++ LeetCode/cpp/BinarySubarraysWithSum.cpp
@@ -1,5 +1,7 @@
 class Solution
 {
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
 public:
     int numSubarraysWithSum(vector<int> &nums, int goal)
     {
@@ -14,5 +16,42 @@ public:
         }
 
         return result;
+    }
+};
+
+/*
+Explaination:
+1. We are given an array of integers nums and an integer goal.
+2. We have to find the number of non-empty subarrays with a sum goal.
+3. We will use a hashmap to store the count of the sum of the subarray.
+4. We will iterate over the array and calculate the sum of the subarray.
+5. We will increment the count of the sum in the hashmap.
+6. We will increment the result by the count of the sum - goal in the hashmap.
+7. Finally, we will return the result.
+*/
+
+// Optimal Solution
+class Solution {
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        auto helper = [&](int x) {
+            if (x < 0)
+                return 0;
+            int l = 0;
+            int sum = 0;
+            int count = 0;
+            for (int r = 0; r < n; r++) {
+                sum += nums[r];
+                while (sum > x) {
+                    sum -= nums[l++];
+                }
+                count += (r-l+1);
+            }
+            return count;
+        };
+        return helper(goal) - helper(goal-1);
     }
 };
