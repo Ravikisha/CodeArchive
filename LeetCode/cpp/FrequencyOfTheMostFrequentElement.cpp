0968a3626d89c1f461096c677aecae4d0c2dc221
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-28 10:51:08 +0530

    Frequency of the Most Frequent Element

---
+++ LeetCode/cpp/FrequencyOfTheMostFrequentElement.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    // Time Complexity: O(nlogn)
+    // Space Complexity: O(1)
+public:
+    int maxFrequency(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        int maxFreq = 0;
+        long long total = 0;
+        int left = 0, right = 0;
+        while (right < nums.size())
+        {
+            total += nums[right];
+            while ((long long)nums[right] * (long long)(right - left + 1) > (long long)total + k)
+            {
+                total -= nums[left++];
+            }
+            maxFreq = max(maxFreq, right - left + 1);
+            right++;
+        }
+        return maxFreq;
+    }
+};
