56d36314a1e9f434b3d99a8fcc7e55ada36340de
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-28 19:58:56 +0530

    Valid Perfect Square

---
+++ LeetCode/cpp/ValidPerfectSquare.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    // Time Complexity: O(logn)
+    // Space Complexity: O(1)
+public:
+    bool isPerfectSquare(int num)
+    {
+        long long low = 1, high = num;
+        while (low <= high)
+        {
+            long long mid = (low + high) >> 1;
+            if (mid * mid == num)
+            {
+                return true;
+            }
+            else if (mid * mid < num)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
