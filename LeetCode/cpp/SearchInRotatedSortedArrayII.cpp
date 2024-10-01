70a959131e3effee78dcd9b05a2ada2f1a624419
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-01 18:41:33 +0530

    Search in Rotated Sorted Array II

---
+++ LeetCode/cpp/SearchInRotatedSortedArrayII.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    // Time Complexity: O(logn)
+    // Space Complexity: O(1)
+public:
+    bool search(vector<int>& nums, int target) {
+      int n = nums.size();
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return true;
+            if(nums[low] < nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid -1;
+                }else{
+                    low = mid+1;
+                }
+            }
+            else if(nums[low] > nums[mid]){
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+            else low++;
+        }
+        return false;  
+    }
+};
