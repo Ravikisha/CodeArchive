608edabc9a505426b65a80bf4b2a10b12cb1e1ba
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-05 23:43:45 +0530

    First Bad Version

---
+++ LeetCode/cpp/FirstBadVersion.cpp
@@ -0,0 +1,26 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution
+{
+    // Time Complexity: O(logn)
+    // Space Complexity: O(1)
+public:
+    int firstBadVersion(int n)
+    {
+        int low = 1, high = n;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid))
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
