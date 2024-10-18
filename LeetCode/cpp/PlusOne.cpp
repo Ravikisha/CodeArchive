a07a1169007ed5795042bbab328be28dce184c7a
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-19 00:17:27 +0530

    66. Plus One

---
+++ LeetCode/cpp/PlusOne.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int N = digits.size();
+        int pos = N - 1;
+        while (pos >= 0 && digits[pos] == 9)
+        {
+            // carry
+            digits[pos] = 0;
+            pos--;
+        }
+
+        if (pos < 0)
+        {
+            digits.insert(digits.begin(), 1);
+        }
+        else
+        {
+            digits[pos]++;
+        }
+
+        return digits;
+    }
+};
