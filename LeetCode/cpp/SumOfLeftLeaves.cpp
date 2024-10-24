ce5eb4ada983c6a431aaa5587d1c1ceb771bdaf6
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-24 22:46:28 +0530

    Sum of Left Leaves

--- LeetCode/cpp/SumOfLeftLeaves.cpp
+++ LeetCode/cpp/SumOfLeftLeaves.cpp
@@ -6,35 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution
 {
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
 public:
-    int ans;
-    void rSumOfLeftLeaves(TreeNode *node, bool isLeft)
-    {
-        // cout << node->val << " " << isLeft << endl;
-        if (node->left == NULL && node->right == NULL && isLeft)
-        {
-            ans += node->val;
-        }
-        if (node->left)
-        {
-            rSumOfLeftLeaves(node->left, true);
-        }
-        if (node->right)
-        {
-            rSumOfLeftLeaves(node->right, false);
-        }
-    }
     int sumOfLeftLeaves(TreeNode *root)
     {
         if (root == NULL)
             return 0;
-
-        ans = 0;
+        int ans = 0;
+        function<void(TreeNode *, bool)> rSumOfLeftLeaves = [&](TreeNode *node, bool isLeft)
+        {
+            if (node->left == NULL && node->right == NULL && isLeft)
+            {
+                ans += node->val;
+            }
+            if (node->left)
+            {
+                rSumOfLeftLeaves(node->left, true);
+            }
+            if (node->right)
+            {
+                rSumOfLeftLeaves(node->right, false);
+            }
+        };
         rSumOfLeftLeaves(root, false);
 
         return ans;
