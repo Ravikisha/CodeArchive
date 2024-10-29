5e6e93dc9ad1af1562530fd47ef68c854a4c944d
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-29 17:09:06 +0530

    Shift 2D Grid

--- LeetCode/cpp/PalindromeNumber.cpp
+++ LeetCode/cpp/PalindromeNumber.cpp
@@ -1,14 +1,19 @@
 // Cpp Solution
-// Time: O(log10(x)), Space: O(1)
-class Solution {
+class Solution
+{
+    // Time Complexity: O(log(n))
+    // Space Complexity: O(1)
 public:
-    bool isPalindrome(int x) {
-        if (x < 0) return false;
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
         long long pali = 0, temp = x;
-        while(temp != 0){
-            pali = pali*10+(temp%10);
+        while (temp != 0)
+        {
+            pali = pali * 10 + (temp % 10);
             temp /= 10;
-        }   
-        return pali==x;
+        }
+        return pali == x;
     }
 };
---
+++ LeetCode/cpp/Shift2DGrid.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    // Time Complexity: O(n*m)
+    // Space Complexity: O(n*m)
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>> &grid, int k)
+    {
+        vector<vector<int>> ans(grid.size(), vector<int>(grid[0].size()));
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                int newcol = (j + k) % m;
+                int newrow = (i + (j + k) / m) % n;
+                ans[newrow][newcol] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
