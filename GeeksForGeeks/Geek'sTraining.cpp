093f2564025fb03639a95516c46bbe41e462805a
ravikishan.69 <ravikishan63392@gmail.com>
2024-12-30 22:39:33 +0530

    Geek's Training

--- GeeksForGeeks/Geek'sTraining.cpp
+++ GeeksForGeeks/Geek'sTraining.cpp
@@ -7,6 +7,7 @@ class Solution
 {
     // Time Complexity: O(n)
     // Space Complexity: O(n)
+    // Memoization based recursive solution
 public:
     int maxMeritPoints(int day, int lastActivity, vector<vector<int>> &arr, vector<vector<int>> &dp)
     {
@@ -39,6 +40,75 @@ public:
     {
         vector<vector<int>> dp(n, vector<int>(4, -1));
         return maxMeritPoints(n - 1, 3, arr, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr;
+        for (int i = 0; i < n; ++i)
+        {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j)
+            {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            arr.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.maximumPoints(arr, n) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    // Tabulation based iterative solution
+public:
+    // Wrapper function
+    int maximumPoints(vector<vector<int>> &arr, int n)
+    {
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] = max(arr[0][1], arr[0][2]);
+        dp[0][1] = max(arr[0][0], arr[0][2]);
+        dp[0][2] = max(arr[0][0], arr[0][1]);
+        dp[0][3] = max(arr[0][0], max(arr[0][1], arr[0][2]));
+
+        for (int day = 1; day < n; day++)
+        {
+            for (int last = 0; last < 4; last++)
+            {
+                dp[day][last] = 0;
+                for (int task = 0; task < 3; task++)
+                {
+                    if (task != last)
+                    {
+                        int point = arr[day][task] + dp[day - 1][task];
+                        dp[day][last] = max(dp[day][last], point);
+                    }
+                }
+            }
+        }
+        return dp[n - 1][3];
     }
 };
 
